adbddd3369157df3aec95e86e2b5eaa57dda55ea
HarshSeth11 <harshsethr@gmail.com>
2024-01-20 22:07:26 +0530

    last commit of the day

--- 1.2-Working-of-React/vite-project/src/main.jsx
+++ 1.2-Working-of-React/vite-project/src/main.jsx
@@ -14,10 +14,20 @@ const MyElement = (
   <h1> Hello World</h1>
 )
 
+const AnotherElement = React.createElement(
+  "a", /* Here we have to provide the tag name */
+  { /* This is the options object which is containing attributes for the tag */
+    href : "https://www.google.com",
+    target : "_blank"
+  },
+  "Click Here to go to Google" /* This is the children that is passed to the element */
+)
+
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
   // <App />     /* It Works */
   // < MyApp />  /* It Works */
   // MyApp()     /* It Works */
   // MyElement   /* It Works */
+  AnotherElement
 )
