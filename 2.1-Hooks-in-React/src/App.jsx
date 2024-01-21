a7cbf1a55988f8c37d4e7d86e85d7f5a5a3fcf53
Harsh seth <68950338+HarshSeth11@users.noreply.github.com>
2024-01-21 16:56:10 +0530

    Update App.jsx

--- 2.1-Hooks-in-React/src/App.jsx
+++ 2.1-Hooks-in-React/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   const [count, setCount] = useState(0);
 
   const incrementCount = () => {
-    /* So here the function calls will be sent in batches. So, react will see them as the same operation and perform it only once. So, the counter will increase by only 1 count.*/
+    /* So here the function calls will be sent in batches. So, react will see them as the same operation and perform it only once.
+    So, the counter will increase by only 1 count.*/
     /*
     setCount(count + 1);
     console.log(count); // 0
@@ -18,9 +19,11 @@ function App() {
     */
 
 
-    /* Now here the logic will be different, it might seem like a same operation but the setCount is passed with a arrow function which will take the previous value of the count and than increment it unlike the previous operation.*/
+    /* Now here the logic will be different, it might seem like a same operation but the setCount is passed with a arrow function
+    which will take the previous value of the count and than increment it unlike the previous operation.*/
 
-    /* This ensures that each setCount call operates on the latest state, avoiding issues with batching. The console.log(count) statements, however, will still print the current value of count (not the updated value), as setState is asynchronous. */
+    /* This ensures that each setCount call operates on the latest state, avoiding issues with batching. 
+    The console.log(count) statements, however, will still print the current value of count (not the updated value), as setState is asynchronous. */
 
     /*
     setCount(prevCount => prevCount + 1);
@@ -33,7 +36,8 @@ function App() {
     console.log(count); // 0
     */
 
-    /* Note : Console log will give the same value as of count at the time of change, as the jobs are first batched and then executed later on this all happens due to performance optimization and Fibre Which is one of the algorithm of the React is responsible for this. */
+    /* Note : Console log will give the same value as of count at the time of change, as the jobs are first batched and 
+    then executed later on this all happens due to performance optimization and Fibre Which is one of the algorithm of the React is responsible for this. */
   }
 
   const decrementCount = () => {
